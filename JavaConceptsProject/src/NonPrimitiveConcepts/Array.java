06427103aa82ced13efc13c8564d6370821662be
mohan <mohan@192.168.0.11>
2017-08-30 20:46:43 +0530

    jj

---
+++ JavaConceptsProject/src/NonPrimitiveConcepts/Array.java
@@ -0,0 +1,12 @@
+package NonPrimitiveConcepts;
+
+public class Array {
+	
+	int[] a = new int[5];
+	int ab[]=new int[5];
+	int b[]= {1,2,3,4,5,6,};
+	int[][] c = new int[3][4];
+	int[][] num = {{1,2},{2,3},{3,4}};
+	
+
+}
---
+++ JavaConceptsProject/src/NonPrimitiveConcepts/Collection.java
@@ -0,0 +1,16 @@
+package NonPrimitiveConcepts;
+
+import java.util.ArrayList;
+
+public class Collection {
+
+	/* collections is an interface , inbuilt in java
+	 * collection is divided in interfaces as  List , set, map , which are interfaces as well
+	 * List has two main classes , Array list and linked list .
+	 * set again has two classes hased set and linked set 
+	 * Map has two classes hash map and linked map 
+	 * 
+	 */
+	ArrayList <String> list = new ArrayList <String>();
+		
+}
